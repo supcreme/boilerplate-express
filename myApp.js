4fc373537105a89f6afab58bac474a468d230984
somic <aoenax@gmail.com>
2021-11-20 15:48:32 +0600

    modify files

--- myApp.js
+++ myApp.js
@@ -3,4 +3,10 @@ var app = express();
 
 console.log('Hello World');
 
+app.get('/', function(req, res) {
+  let absolutePath = __dirname +
+      '/views/index.html';
+  res.sendFile(absolutePath);
+});
+
 module.exports = app;
